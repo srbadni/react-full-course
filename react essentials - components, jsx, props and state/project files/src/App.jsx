5ad1176f6d61354ad5080effea3e1151c923939c
alireza sarabadani <srbadni@gmail.com>
2025-08-14 16:23:50 +0330

    move project files

---
+++ react essentials - components, jsx, props and state/project files/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="react core concepts" />
+        <h1>مبانی ری‌اکت</h1>
+        <p>
+            مفاهیم کلیدی React که توی هر اپی که می‌سازید بهشون نیاز دارین
+        </p>
+      </header>
+      <main>
+        <h2>بریم شروع کنیم!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
