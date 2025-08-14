d1741a5b2c9af61678924deab69230377dbc8da7
alireza sarabadani <srbadni@gmail.com>
2025-08-14 19:55:04 +0330

    Outputting Dynamic Values

--- react essentials - components, jsx, props and state/project files/src/App.jsx
+++ react essentials - components, jsx, props and state/project files/src/App.jsx
@@ -1,9 +1,17 @@
+const reactDescriptions = ['مقدماتی', 'کلیدی', 'اساسی'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
   return (<header>
     <img src="src/assets/react-core-concepts.png" alt="react core concepts" />
     <h1>مبانی ری‌اکت</h1>
     <p>
-      مفاهیم کلیدی React که توی هر اپی که می‌سازید بهشون نیاز دارین
+      مفاهیم {description} React که توی هر اپی که می‌سازید بهشون نیاز دارین
     </p>
   </header>)
 }
