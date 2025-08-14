c35e604c79cb457cf7b1c5002d29aad3c19fd138
alireza sarabadani <srbadni@gmail.com>
2025-08-14 17:40:26 +0330

    Creating & Using a First Custom Component

--- react essentials - components, jsx, props and state/project files/src/App.jsx
+++ react essentials - components, jsx, props and state/project files/src/App.jsx
@@ -1,13 +1,17 @@
+function Header() {
+  return (<header>
+    <img src="src/assets/react-core-concepts.png" alt="react core concepts" />
+    <h1>مبانی ری‌اکت</h1>
+    <p>
+      مفاهیم کلیدی React که توی هر اپی که می‌سازید بهشون نیاز دارین
+    </p>
+  </header>)
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="react core concepts" />
-        <h1>مبانی ری‌اکت</h1>
-        <p>
-            مفاهیم کلیدی React که توی هر اپی که می‌سازید بهشون نیاز دارین
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>بریم شروع کنیم!</h2>
       </main>
