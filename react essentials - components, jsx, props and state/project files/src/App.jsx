7aa3c89954eb96fe46b9f21bee9099d074826a97
alireza sarabadani <srbadni@gmail.com>
2025-08-21 14:38:01 +0330

    load image file dynamically

--- react essentials - components, jsx, props and state/project files/src/App.jsx
+++ react essentials - components, jsx, props and state/project files/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ['مقدماتی', 'کلیدی', 'اساسی'];
 
 function getRandomInt(max) {
@@ -8,7 +10,7 @@ function Header() {
   const description = reactDescriptions[getRandomInt(2)];
 
   return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="react core concepts" />
+    <img src={reactImg} alt="react core concepts" />
     <h1>مبانی ری‌اکت</h1>
     <p>
       مفاهیم {description} React که توی هر اپی که می‌سازید بهشون نیاز دارین
